b6c2acfeb0346967f0ff6d34fae8c3bfc1acec3c
Kenny Liew <khl.swift@gmail.com>
2019-01-15 16:11:09 +0800

    don't use page not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AlbumsComponent } from './album/albums.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SongsComponent } from './song/songs.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/albums', pathMatch: 'full' },
   { path: 'albums', component: AlbumsComponent },
   { path: 'songs',  component: SongsComponent },
   { path: 'album', loadChildren: './album/album.module#AlbumModule' },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '', redirectTo: '/albums', pathMatch: 'full' },
+  { path: '**', redirectTo: '/albums', pathMatch: 'full' }
 ];
 
 @NgModule({
