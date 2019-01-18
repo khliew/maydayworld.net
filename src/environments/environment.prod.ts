0f5001d9282a13a5951a6e84d53490edede40871
Kenny Liew <khl.swift@gmail.com>
2019-01-18 11:44:56 +0800

    update api base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://api.maydayworld.com'
+  apiBaseUrl: 'https://api.maydayworld.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://api.maydayworld.com'
+  apiBaseUrl: 'http://localhost:3001'
 };
 
 /*
