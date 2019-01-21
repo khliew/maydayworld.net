e5b4adf0dad46a0c183b31f7d266f5f754565b4b
Kenny Liew <khl.swift@gmail.com>
2019-01-22 01:07:57 +0800

    add google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material';
+import { NavigationEnd, Router } from '@angular/router';
 import { SidenavService } from './service/sidenav.service';
 
 @Component({
@@ -10,9 +11,17 @@ import { SidenavService } from './service/sidenav.service';
 export class AppComponent implements OnInit {
   @ViewChild(MatSidenav) sidenav: MatSidenav;
 
-  constructor(private sidenavService: SidenavService) { }
+  constructor(private router: Router, private sidenavService: SidenavService) {
+  }
 
   ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+      }
+    });
+
     this.sidenavService.open
       .subscribe(open => {
         if (open) {
