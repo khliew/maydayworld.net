baf834d56fd30b1f81985257ed7d59e56fef285c
Kenny Liew <khl.swift@gmail.com>
2019-01-23 23:00:13 +0800

    add spec for sidenav service

---
+++ src/app/services/sidenav.service.spec.ts
@@ -0,0 +1,21 @@
+import { SidenavService } from './sidenav.service';
+
+describe('SidenavService', () => {
+  let service: SidenavService;
+  beforeEach(() => service = new SidenavService());
+
+  it('#setEnabled should set enabled value',
+    () => {
+      service.setEnabled(true);
+      expect(service.enabled).toBe(true);
+    }
+  );
+
+  it('enable$ should emit value from #setEnabled', (done: DoneFn) => {
+    service.enable$.subscribe(value => {
+      expect(value).toBe(true);
+      done();
+    });
+    service.setEnabled(true);
+  });
+});
