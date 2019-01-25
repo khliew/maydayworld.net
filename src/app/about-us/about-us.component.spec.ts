ab4cca5d57567ccfd3a7749a491d120685782c49
Kenny Liew <khl.swift@gmail.com>
2019-01-25 10:56:26 +0800

    add about us component spec

---
+++ src/app/about-us/about-us.component.spec.ts
@@ -0,0 +1,21 @@
+import { AboutUsComponent } from './about-us.component';
+
+describe('DataService', () => {
+  let sidenavService: { setEnabled: jasmine.Spy };
+  let titleService: { resetTitle: jasmine.Spy };
+
+  beforeEach(() => {
+    sidenavService = jasmine.createSpyObj('SidenavService', ['setEnabled']);
+    titleService = jasmine.createSpyObj('TitleService', ['resetTitle']);
+  });
+
+  it('should hide the sidenav', () => {
+    const comp = new AboutUsComponent(<any> titleService, <any> sidenavService);
+    expect(sidenavService.setEnabled).toHaveBeenCalledWith(false);
+  });
+
+  it('should reset the document title', () => {
+    const comp = new AboutUsComponent(<any> titleService, <any> sidenavService);
+    expect(titleService.resetTitle).toHaveBeenCalled();
+  });
+});
