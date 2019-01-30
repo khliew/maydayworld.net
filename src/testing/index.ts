4925c71f179044c8e037ae10117d3ca45da959f2
Kenny Liew <khl.swift@gmail.com>
2019-01-30 15:59:26 +0800

    add log in component spec

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -22,3 +22,15 @@ export function click(el: DebugElement | HTMLElement, eventObj: any = ButtonClic
     el.triggerEventHandler('click', eventObj);
   }
 }
+
+/**
+ * Create custom DOM event the old fashioned way
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/API/Event/initEvent
+ * Although officially deprecated, some browsers (phantom) don't accept the preferred "new Event(eventName)"
+ */
+export function newEvent(eventName: string, bubbles = false, cancelable = false) {
+  const evt = document.createEvent('CustomEvent');  // MUST be 'CustomEvent'
+  evt.initCustomEvent(eventName, bubbles, cancelable, null);
+  return evt;
+}
