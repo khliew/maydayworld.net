edc2cc33fc153f22a2e763d6f66a83e86b49b7f6
Kenny Liew <khl.swift@gmail.com>
2019-06-25 13:13:56 +0800

    create admin-only material module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,10 +8,12 @@ import { AdminService } from './admin.service';
 import { AlbumCreatorComponent } from './album/album-creator.component';
 import { DiscographyCreatorComponent } from './discography/discography-creator.component';
 import { SongCreatorComponent } from './song/song-creator.component';
+import { AdminMaterialModule } from './admin-material.module';
 
 @NgModule({
   imports: [
     SharedModule,
+    AdminMaterialModule,
     MatDatepickerModule,
     MatMomentDateModule,
     AdminRoutingModule
